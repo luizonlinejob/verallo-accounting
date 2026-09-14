ae059f21a0cad32b49f3edfc4736fd3f3f2b36aa
Luis <luizonlinejob@gmail.com>
2026-09-14 14:33:11 +0800

    Force HTTPS scheme in production for Render

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // 1. Idugang kini sa taas
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // 2. Idugang kini aron ma-force ang HTTPS sa Render
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
